dfbc9560e7ce315d9b03cf0c589e31a6d7c6855f
opauloh <paulo.scape@gmail.com>
2020-05-18 23:08:25 -0300

    Badge Component

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { Color } from '../../helpers/dictionary';
+
+export const Wrapper = styled.span`
+    font-family: SF Pro Text;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 0.625rem;
+    line-height: 0.937rem;
+    border-radius: 2px;
+    padding: 1px 4px;
+    color: #4F4F4F;
+    background-color: ${props => (Color(props.color))};
+`;
+
+type BadgeProps = {
+    color: string
+}
+
+export const Badge: FunctionComponent<BadgeProps> = ({ color, children }) => <Wrapper color={color}>
+    {children}
+</Wrapper>;
+
+export default Badge;
