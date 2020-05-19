c3ba2133baec190996f32048ad0bdc2e67dbadca
opauloh <paulo.scape@gmail.com>
2020-05-18 23:08:32 -0300

    Visualizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Badge from './components/Badge';
+import Pallete from './components/Pallete';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "1rem" }}>
+      <h1>Boring Design System <button >Toggle Dark Mode</button></h1>
+
+      <h2>Colors</h2>
+      <div style={{ display: "flex" }}>
+        <div style={{ textAlign: "center", marginRight: "2rem" }}>
+          <div style={{ marginBottom: "0.5rem" }}>yellow</div>
+          <Pallete color="yellow" />
+        </div>
+        <div style={{ textAlign: "center", marginRight: "2rem" }}>
+          <div style={{ marginBottom: "0.5rem" }}>purple</div>
+          <Pallete color="purple" />
+        </div>
+        <div style={{ textAlign: "center", marginRight: "2rem" }}>
+          <div style={{ marginBottom: "0.5rem" }}>blue</div>
+          <Pallete color="blue" />
+        </div>
+        <div style={{ textAlign: "center", marginRight: "2rem" }}>
+          <div style={{ marginBottom: "0.5rem" }}>green</div>
+          <Pallete color="green" />
+        </div>
+
+      </div>
+      <hr />
+      <h2>Badges</h2>
+      <p>
+        Usage:
+        <pre>
+          {`<Badge color="yellow">New</Badge>`}<br />
+          {`<Badge color="purple">Producing content</Badge>`}<br />
+          {`<Badge color="blue">Reviewing</Badge>`}<br />
+          {`<Badge color="green">Approved</Badge>`}
+        </pre>
+      </p>
+      <div>
+        <div><Badge color="yellow">New</Badge></div>
+        <div><Badge color="purple">Producing content</Badge></div>
+        <div><Badge color="blue">Reviewing</Badge></div>
+        <div><Badge color="green">Approved</Badge></div>
+      </div>
+      <hr />
     </div>
   );
 }
