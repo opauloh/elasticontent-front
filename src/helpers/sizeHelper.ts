ef1673000d7627d059ebb133b04cc0d3ed5e5066
opauloh <paulo.scape@gmail.com>
2020-06-24 00:28:04 -0300

    sizeHelper

---
+++ src/helpers/sizeHelper.ts
@@ -0,0 +1,21 @@
+import spacing from '../tokens/spacing';
+import { SizeType } from '../types/SizeType';
+
+export const getSize = (size: SizeType) => {
+    switch (size) {
+        case 'xxs':
+            return spacing[`$spacing-xxs`];
+        case 'xs':
+            return spacing[`$spacing-xs`];
+        case 'sm':
+            return spacing[`$spacing-sm`];
+        case 'md':
+            return spacing[`$spacing-md`];
+        case 'lg':
+            return spacing[`$spacing-lg`];
+        case 'xl':
+            return spacing[`$spacing-xl`];
+        default:
+            return spacing[`$spacing-md`];
+    }
+};
