010f02945d9fa98330dda4d176e7af122a0b2ae0
opauloh <paulo.scape@gmail.com>
2020-07-06 16:58:59 -0300

    Card List Component

---
+++ src/components/Cardlist/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Styled } from './styled';
+
+const Cardlist: React.FC = () => {
+  return (
+    <Styled.Cardlist>
+      {/* <Card> */}
+    </Styled.Cardlist>
+  )
+}
+
+export default Cardlist;
---
+++ src/components/Cardlist/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Cardlist = styled.div`
+    width: 304px;
+    height: 100vh;
+    background-color: var(--theme-bg-color-secondary);
+`;
+
+export const Styled = {
+  Cardlist
+}
