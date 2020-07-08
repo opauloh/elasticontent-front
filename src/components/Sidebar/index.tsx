56a19aaa2eb7790cbbc087f721dfb261920b8d68
opauloh <paulo.scape@gmail.com>
2020-07-08 00:42:21 -0300

    Sidebar Component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Styled } from "./styled";
+import AvatarName from '../base/Avatar/AvatarName';
+import Menu from '../Menu';
 
 const Sidebar: React.FC = () => {
     return (
         <Styled.Sidebar>
-            lol
+            <Styled.AvatarWrapper>
+                <AvatarName size="lg" src="https://www.pathstone.com/app/uploads/2020/02/avatar-placeholder.png">Drieli Lopes</AvatarName>
+            </Styled.AvatarWrapper>
+            <Menu />
         </Styled.Sidebar>
     )
 }
--- src/components/Sidebar/styled.ts
+++ src/components/Sidebar/styled.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import spacing from '../../tokens/spacing';
 
 const Sidebar = styled.div`
     width: 254px;
-    height: 100%;
-    background-color: var(--theme-bg-color-secondary);
+    height: 100vh;
+    background-color: var(--theme-bg-color-tertiary);
+`;
+const AvatarWrapper = styled.div`
+    padding-top: ${spacing["$spacing-sm"]};
+    padding-left: ${spacing["$spacing-md"]};
+    padding-bottom: ${spacing["$spacing-sm"]};
 `;
 
 export const Styled = {
-    Sidebar
+    Sidebar,
+    AvatarWrapper
 }
