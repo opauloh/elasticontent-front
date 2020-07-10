6f98d9e7bf08b943513d32e3d3f991f93e0dd533
opauloh <paulo.scape@gmail.com>
2020-07-10 15:16:45 -0300

    Card Component

--- src/components/base/Card/index.tsx
+++ src/components/base/Card/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Styled } from './styled';
+import { CardType } from '../../../types/CardType';
+import AvatarImg from '../Avatar/AvatarImg';
+import { IconStar } from '../../icons';
+import Status from '../Status';
 
-const Card: React.FC = () => {
+type CardProps = {
+  card: CardType
+}
+
+const Card: React.FC<CardProps> = ({ card }) => {
   return (
     <Styled.Card>
-      {/* <Card> */}
+      <Styled.Avatar>
+        <AvatarImg src={card.responsible.image} size="md" />
+      </Styled.Avatar>
+      <Styled.Company>{card.company}</Styled.Company>
+      <Styled.DateTime>09:45</Styled.DateTime>
+      <Styled.Title>Briefing mês de abril</Styled.Title>
+      <Styled.Status>
+        <Status />
+      </Styled.Status>
+      <Styled.Favorite>
+        <IconStar />
+      </Styled.Favorite>
+      <Styled.Description>{card.description}</Styled.Description>
     </Styled.Card>
   )
 }
--- src/components/base/Card/styled.ts
+++ src/components/base/Card/styled.ts
@@ -1,26 +1,79 @@
 import styled from 'styled-components';
 import spacing from '../../../tokens/spacing';
+import fontSizes from '../../../tokens/fontSizes';
+import colors from '../../../tokens/colors';
+import fontWeights from '../../../tokens/fontWeights';
 
 const Card = styled.div`
-    width: 100%;
     padding: ${spacing["$spacing-md"]} ${spacing["$spacing-sm"]};
     display: grid;
     align-items: center;
-    grid-template-columns: ${spacing["$spacing-md"]} max-content ${spacing["$spacing-lg"]};
-    grid-template-rows: repeat(3, 20px);
+    grid-template-columns: ${spacing["$spacing-md"]} 1fr max-content ${spacing["$spacing-lg"]};
+    grid-template-rows: repeat(3, auto);
     grid-template-areas:
-        "top-left top-line top-date top-right"
-        "middle-left middle-line middle-line middle-line"
-        "bottom-left bottom-line bottom-line bottom-right";
-    grid-row-gap: 2px;
-    grid-column-gap: 6px;
+        "top-left top-line top-line top-right"
+        "middle-left middle-line middle-status middle-right"
+        "bottom-left bottom-line bottom-line bottom-line";
+    grid-row-gap: ${spacing["$spacing-xs"]};
+    grid-column-gap: ${spacing["$spacing-sm"]};
 }
 `;
 
 const Avatar = styled.div`
-
+  grid-column: top-left;
+  grid-row: top-left / middle-left;
+  vertical-align: top;
+  float: left;
+  align-self: self-start;
+}
+`;
+const Company = styled.div`
+  display: flex;
+  -webkit-box-align: start;
+  align-items: start;
+  min-width: 0px;
+  grid-area: top-line / top-line / top-line / top-line;
+  font-size: ${fontSizes["$font-size-xxxs"]};
+  align-self: start;
 `;
 
+const DateTime = styled.div`
+  grid-area: top-right / top-right / top-right / top-right;
+  font-size: ${fontSizes["$font-size-xxxxs"]};
+`;
+
+const Title = styled.div`
+  grid-area: middle-line / middle-line / middle-line / middle-line;
+  color: ${colors["$gray-80"]};
+  font-weight: ${fontWeights["$font-weight-bold"]};
+`;
+
+const Status = styled.div`
+  grid-area: middle-status / middle-status / middle-status / middle-status;
+`;
+
+const Favorite = styled.div`
+  grid-area: bottom-left / bottom-left / bottom-left / bottom-left;
+  color: ${colors["$yellow-20"]};
+  text-align: center;
+`;
+
+const Description = styled.div`
+  grid-area: bottom-line / bottom-line / bottom-line / bottom-line;
+  color: ${colors["$gray-60"]};
+  font-size: ${fontSizes["$font-size-xxs"]};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
 export const Styled = {
-  Card
+  Card,
+  Avatar,
+  Company,
+  Description,
+  DateTime,
+  Title,
+  Status,
+  Favorite
 }
