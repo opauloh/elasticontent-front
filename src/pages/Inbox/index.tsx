db696e72d33cebcfe3308cd80a101c02ad7e3eec
opauloh <paulo.scape@gmail.com>
2020-07-10 15:16:21 -0300

    Inbox

--- src/pages/Inbox/index.tsx
+++ src/pages/Inbox/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
-
+import Cardlist from '../../components/Cardlist';
+import { Styled } from './styled';
 
 const Inbox: React.FC = () => {
     return (
-        <>
+        <Styled.Inbox>
             <Sidebar />
-        </>
+            <Cardlist cards={[
+                {
+                    id: '111',
+                    company: 'Electrolux',
+                    responsible: {
+                        name: 'Drieli Lopes',
+                        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvL8VK49O_3-UUwrUQhbfs_pnXTBGBYjm40g&usqp=CAU',
+                    },
+                    status: 'pending',
+                    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
+                },
+                {
+                    id: '2',
+                    company: 'Electrolux',
+                    responsible: {
+                        name: 'Drieli Lopes',
+                        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvL8VK49O_3-UUwrUQhbfs_pnXTBGBYjm40g&usqp=CAU',
+                    },
+                    status: 'pending',
+                    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
+                }
+            ]} />
+        </Styled.Inbox>
     )
 }
 
---
+++ src/pages/Inbox/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Inbox = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: auto auto auto auto minmax(540px, 1fr) auto;
+  grid-template-rows: 100%;
+  min-height: 250px;
+  height: 100%;
+`;
+
+export const Styled = {
+  Inbox
+}
