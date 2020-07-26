7a0b70deb0d20f7dbd6b3314e24f6a54988a2e04
opauloh <paulo.scape@gmail.com>
2020-07-26 16:14:54 -0300

    refactory Avatar component

--- src/components/app/Avatar/AvatarName/index.tsx
+++ src/components/app/Avatar/AvatarName/index.tsx
@@ -1,34 +1,23 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
-import fontWeights from '../../../../tokens/fontWeights';
-import fontSizes from '../../../../tokens/fontSizes';
 import { AvatarImg } from '../AvatarImg';
-import spacingInline from '../../../../tokens/spacingInline';
 import { SizeType } from '../../../types/SizeType';
-
-const Name = styled.span`
-    font-weight: ${fontWeights["$font-weight-medium"]};
-    font-size: ${fontSizes["$font-size-xxs"]};
-    color: var(--theme-text-secondary);
-    margin-left: ${spacingInline["$spacing-inline-xxxs"]};
-`;
-const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
+import { Styled } from './styled';
 
 type AvatarProps = {
-    src: string,
-    size?: SizeType,
-    children: string
-}
+  src: string;
+  size?: SizeType;
+  children: string;
+};
 
-export const AvatarName: FunctionComponent<AvatarProps> = ({ src, size, children }) => (
-    <Wrapper>
-        <AvatarImg src={src} size={size} />
-        <Name>{children}</Name>
-    </Wrapper>
+export const AvatarName: FunctionComponent<AvatarProps> = ({
+  src,
+  size,
+  children
+}) => (
+  <Styled.Wrapper>
+    <AvatarImg src={src} size={size} />
+    <Styled.Name>{children}</Styled.Name>
+  </Styled.Wrapper>
 );
 
 export default AvatarName;
---
+++ src/components/app/Avatar/AvatarName/styled.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import fontWeights from '../../../../tokens/fontWeights';
+import fontSizes from '../../../../tokens/fontSizes';
+import spacingInline from '../../../../tokens/spacingInline';
+import colors from '../../../../tokens/colors';
+
+const Name = styled.span`
+  font-weight: ${fontWeights['$font-weight-semibold']};
+  font-size: ${fontSizes['$font-size-xxs']};
+  color: ${colors['$color-black']};
+  margin-left: ${spacingInline['$spacing-inline-xxxs']};
+  opacity: 0.75;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Styled = {
+  Name,
+  Wrapper
+};
