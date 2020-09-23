733c737852d74a4e5ba6d97e4259cd8a4d21dbfb
opauloh <paulo.scape@gmail.com>
2020-09-23 17:54:10 -0300

    change useEffect to useLayoutEffect

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,7 +3,7 @@ import React from 'react';
 const useTheme = () => {
   const theme = React.useRef('light');
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     setTheme();
   }, []);
 
